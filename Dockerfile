5ebd55c45d8d3c64a0bc569c3df9701b8282fcfe
gleb165 <83755952+gleb165@users.noreply.github.com>
2024-03-03 20:19:01 +0200

    next commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.12
+# Set working directory to /usr/src/app
+WORKDIR /usr/src/app
+# Copy the contents of the current local directory into the container’s working directory
+ADD . /usr/src/app
+# Run a command
+CMD ["python", "hello.py"]
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
-
+from todo import todo_router
 app = FastAPI()
-
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def welcome() -> dict:
+    return {
+    "message": "Hello World"
+    }
+app.include_router(todo_router)
