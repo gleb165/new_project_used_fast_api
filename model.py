284263a1098717d6c36cdd0165cf7c6c9347a682
gleb165 <83755952+gleb165@users.noreply.github.com>
2024-03-04 15:30:13 +0200

    add Jinja and change file

--- model.py
+++ model.py
@@ -1,10 +1,19 @@
+from typing import List, Optional
+
+from fastapi import Form
 from pydantic import BaseModel
-from typing import List
 
 
 class Todo(BaseModel):
-    id: int
+    id: int | None = None
     item: str
+
+    @classmethod
+    def as_form(
+            cls,
+            item: str = Form(...)
+    ):
+        return cls(item=item)
 
     class Config:
         schema_extra = {
